2702add0135c5ddf3b476243f682cf412b1638a9
dctime <daniel.chu916@gmail.com>
2023-02-27 23:42:31 +0800

    Feature: Add sound and disapear when meet eulers number

---
+++ src/main/java/net/dctime/lawsandtheorems/nature_log_item/server/NatureLogForgeServerEvent.java
@@ -0,0 +1,71 @@
+package net.dctime.lawsandtheorems.nature_log_item.server;
+
+
+import java.util.List;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.dctime.lawsandtheorems.LawsAndTheorems;
+import net.dctime.lawsandtheorems.register.ModItems;
+import net.dctime.lawsandtheorems.register.ModSoundEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.AABB;
+import net.minecraftforge.event.TickEvent.ServerTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = LawsAndTheorems.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class NatureLogForgeServerEvent
+{
+    @SubscribeEvent
+    public static void natureLogItemEntityInWorld(ServerTickEvent event)
+    {
+        for (ServerLevel level : event.getServer().getAllLevels())
+        {
+            if (!level.isClientSide())
+            {
+                for (Entity entity : level.getAllEntities())
+                {
+                    if (entity instanceof ItemEntity && ((ItemEntity) entity).getItem().is(ModItems.NATURE_LOG.get()))
+                    {
+                        if ((int)(Math.random()*100) == 0)
+                        {
+                            entity.playSound(ModSoundEvents.NATURE_LOG_SPLASHING.get());
+                        }
+                        
+                        List<Entity> nearbyEntities = entity.level.getEntities
+                        (
+                            entity,
+                            new AABB(entity.getX()-0.3, entity.getY()-0.3, entity.getZ()-0.3, entity.getX()+0.3, entity.getY()+0.3, entity.getZ()+0.3),
+                            (player) -> true
+                        );
+
+                        for (int index = 0; index < nearbyEntities.size(); index++)
+                        {
+                            if (nearbyEntities.get(index) instanceof ItemEntity && ((ItemEntity)nearbyEntities.get(index)).getItem().is(ModItems.THE_NUMBER_E.get()))
+                            {
+                                ItemEntity eulers_number_in_reaction = ((ItemEntity) nearbyEntities.get(index));
+                                ItemStack eulers_number_after_reaction = eulers_number_in_reaction.copy().getItem();
+                                eulers_number_after_reaction.shrink(1);
+                                eulers_number_in_reaction.setItem(eulers_number_after_reaction);
+
+                                ItemEntity natural_log_in_reaction = ((ItemEntity) entity);
+                                ItemStack natural_log_after_reaction = natural_log_in_reaction.copy().getItem();
+                                natural_log_after_reaction.shrink(1);
+                                natural_log_in_reaction.setItem(natural_log_after_reaction);
+                                
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/net/dctime/lawsandtheorems/register/ModSoundEvents.java
+++ src/main/java/net/dctime/lawsandtheorems/register/ModSoundEvents.java
@@ -1,6 +1,7 @@
 package net.dctime.lawsandtheorems.register;
 
 import net.dctime.lawsandtheorems.LawsAndTheorems;
+import net.minecraft.client.resources.sounds.Sound;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,6 +17,10 @@ public class ModSoundEvents
     public static final RegistryObject<SoundEvent> EULARS_NUMBER_BURNING = 
         SOUND_EVENTS.register("eulars_number_burning", 
         () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(LawsAndTheorems.MODID, "eulars_number_burning")));
+
+    public static final RegistryObject<SoundEvent> NATURE_LOG_SPLASHING =
+        SOUND_EVENTS.register("nature_log_splashing",
+        () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(LawsAndTheorems.MODID, "nature_log_splashing")));
 
     public static void register(IEventBus eventBus)
     {
